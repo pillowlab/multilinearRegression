e51a0fbe1efb46d9efe91ea58a849661b6b1619d
jpillow <jpillow@gmail>
2021-02-23 16:26:39 -0500

    cleanup demo2

--- demo2_testMixedLinearBilinearRegress.m
+++ demo2_testMixedLinearBilinearRegress.m
@@ -16,17 +16,21 @@ nwtot = nwbi+nwlin; % total number of filter elements
 iibi = floor(nwlin/2)+(1:nwbi);  % indices for bilinear elements
 iilin = setdiff(1:nwtot,iibi);   % indices for linear elements
 
-% Make filters
-A = gsmooth(randn(nt,rnk),2); % temporal filters
-B = gsmooth(randn(nx,rnk),2)'; % spatial filters
+% ----------------------------------------------
+% Make true filters
+
+wt = gsmooth(randn(nt,rnk),2); % temporal filters
+wx = gsmooth(randn(nx,rnk),2)'; % spatial filters
 wlin = gsmooth(randn(nwlin,1),10); % purely linear component
 
-wbi = A*B;  % bilnear filter
+wbi = wt*wx;  % bilinear filter
 wtrue = zeros(nwtot,1);  % composite filter
 wtrue(iibi) = vec(wbi); % the regression coefficients
 wtrue(iilin) = wlin;
 
+% ----------------------------------------------
 % Generate training data
+
 nstim = 5000; % number of stimuli
 signse = 10;  % stdev of observation noise
 X = randn(nstim,nwtot); % stimuli
@@ -36,16 +40,23 @@ Y = X*wtrue + randn(nstim,1)*signse; % observations
 XX = X'*X;
 XY = X'*Y;
 
+% ----------------------------------------------
 % Estimate by Coordinate Ascent 
 lambda = 10;  % ridge parameter
 tic;
 [what1,wt1,wx1,wlin1] = bilinearMixRegress_coordAscent(XX,XY,[nt,nx],rnk,iibi,lambda);
 t1 = toc;
 
+% ----------------------------------------------
 % Estimate by Gradient Ascent
 tic;
 [what2,wt2,wx2,wlin2] = bilinearMixRegress_grad(XX,XY,[nt,nx],rnk,iibi,lambda);
 t2 = toc;
+
+
+% ----------------------------------------------
+% Make plots & Report Error 
+% ----------------------------------------------
 
 % Report timings
 fprintf('\nMixed bilinear and linear regression:\n');
@@ -53,16 +64,14 @@ fprintf('-------------------------------------\n');
 fprintf('computation time (coordinate ascent): %f\n', t1);
 fprintf('computation time (joint ascent):      %f\n', t2);
 
-
-% Assemble full weights for both cases
-
-% Make plots
+% Make plot: linear filters
 subplot(221);
 plot(1:nwlin,wlin,1:nwlin,wlin1,1:nwlin,wlin2, '--', 'linewidth', 2);
 legend('true','estim 1 (coord asc.)', 'estim 2 (joint asc.)'); box off;
 xlabel('coefficient #');
 title('linear weights');
 
+% Make plots: bilinearly parametrized filters
 subplot(234); imagesc(wbi);
 title('true bilinear wts'); box off;
 ylabel('time (bins)'); xlabel('space (bins)');
@@ -70,12 +79,6 @@ subplot(235); imagesc(wt1*wx1); box off;
 title('bilin estim (coord. asc.)');
 subplot(236); imagesc(wt2*wx2); box off;
 title('bilin estim (joint asc.)');
-
-
-
-%plot([wtrue what1 what2]);
-%subplot(212);
-%imagesc([wbi, wt*wx, wt2*wx2]);
 
 % Compute R^2 between true and estimated weights
 msefun = @(x,y)(mean((x-y).^2));
